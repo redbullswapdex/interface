7d29566c677792e64da3748b9880159e8136f5de
redbullswapdex <81262588+redbullswapdex@users.noreply.github.com>
2022-10-17 01:01:03 +0200

    Add files via upload

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -6,7 +6,7 @@ import { abi as MERKLE_DISTRIBUTOR_ABI } from '@uniswap/merkle-distributor/build
 import { ChainId, WETH } from '@uniswap/sdk'
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { useMemo } from 'react'
-import { GOVERNANCE_ADDRESS, MERKLE_DISTRIBUTOR_ADDRESS, UNI, LAIR_ADDRESS, QUICK_ADDRESS } from '../constants'
+import { GOVERNANCE_ADDRESS, MERKLE_DISTRIBUTOR_ADDRESS, UNI, HOLE_ADDRESS, QUICK_ADDRESS } from '../constants'
 import {
   ARGENT_WALLET_DETECTOR_ABI,
   ARGENT_WALLET_DETECTOR_MAINNET_ADDRESS
@@ -23,7 +23,7 @@ import { V1_EXCHANGE_ABI, V1_FACTORY_ABI, V1_FACTORY_ADDRESSES } from '../consta
 import { getContract } from '../utils'
 import { useActiveWeb3React } from './index'
 import { Web3Provider } from '@ethersproject/providers'
-import { abi as LairABI } from '../abis/DragonLair.json'; 
+import { abi as HoleABI } from '../abis/BlackHole.json'; 
 const Web3HttpProvider = require('web3-providers-http');
 
 const providers = new Array();
@@ -102,8 +102,8 @@ function useContract(address: string | undefined, ABI: any, withSignerIfPossible
   }, [address, ABI, library, withSignerIfPossible, account])
 }
 
-export function useLairContract(): Contract | null {
-  return useContract(LAIR_ADDRESS, LairABI, true)
+export function useHoleContract(): Contract | null {
+  return useContract(HOLE_ADDRESS, HoleABI, true)
 }
 
 export function useQUICKContract(): Contract | null {
